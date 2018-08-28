11629a0c82734dcc55062abc1768fab0f308a437
Bala Murali krishna polisetti <muralikrishna4400@hotmail.com>
2018-08-28 00:12:34 -0400

    sample page setup
    Signed-off-by: Bala Murali krishna polisetti <muralikrishna4400@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TabNavComponent } from './tab-nav/tab-nav.component';
+import { ContentvComponent } from './contentv/contentv.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TabNavComponent,
+    ContentvComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/contentv/contentv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contentv',
+  templateUrl: './contentv.component.html',
+  styleUrls: ['./contentv.component.css']
+})
+export class ContentvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
